06d9f4c79d374b8bd346965d0c7bc64d10d3fb1d
Luke Steven John Case <rc0032733@student.riversidecollege.ac.uk>
2019-01-14 15:50:48 +0000

    calculate 2-5 resistors

--- Asignment2/Form1.Designer.cs
+++ Asignment2/Form1.Designer.cs
@@ -46,6 +46,12 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -55,11 +61,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(154, 152);
+            this.button1.Location = new System.Drawing.Point(260, 147);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Calculate";
+            this.button1.Text = "Calculate2";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -69,7 +75,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "0";
             // 
             // textBox2
             // 
@@ -77,7 +82,6 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 5;
-            this.textBox2.Text = "0";
             // 
             // textBox3
             // 
@@ -85,7 +89,6 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 6;
-            this.textBox3.Text = "0";
             // 
             // textBox4
             // 
@@ -93,7 +96,6 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 7;
-            this.textBox4.Text = "0";
             // 
             // textBox5
             // 
@@ -101,7 +103,6 @@
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 8;
-            this.textBox5.Text = "0";
             // 
             // textBox6
             // 
@@ -109,7 +110,6 @@
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 20);
             this.textBox6.TabIndex = 14;
-            this.textBox6.Text = "0";
             // 
             // textBox7
             // 
@@ -117,7 +117,6 @@
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(100, 20);
             this.textBox7.TabIndex = 13;
-            this.textBox7.Text = "0";
             // 
             // textBox8
             // 
@@ -125,7 +124,6 @@
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(100, 20);
             this.textBox8.TabIndex = 12;
-            this.textBox8.Text = "0";
             // 
             // textBox9
             // 
@@ -133,7 +131,6 @@
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(100, 20);
             this.textBox9.TabIndex = 11;
-            this.textBox9.Text = "0";
             // 
             // textBox10
             // 
@@ -141,22 +138,21 @@
             this.textBox10.Name = "textBox10";
             this.textBox10.Size = new System.Drawing.Size(100, 20);
             this.textBox10.TabIndex = 10;
-            this.textBox10.Text = "0";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(154, 291);
+            this.button2.Location = new System.Drawing.Point(260, 298);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 23);
             this.button2.TabIndex = 9;
-            this.button2.Text = "Calculate";
+            this.button2.Text = "Calculate2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(158, 77);
+            this.label1.Location = new System.Drawing.Point(151, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 15;
@@ -165,7 +161,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(170, 215);
+            this.label2.Location = new System.Drawing.Point(151, 215);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 16;
@@ -174,7 +170,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(684, 231);
+            this.label3.Location = new System.Drawing.Point(684, 238);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(42, 13);
             this.label3.TabIndex = 17;
@@ -183,17 +179,83 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(684, 96);
+            this.label4.Location = new System.Drawing.Point(684, 100);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(42, 13);
             this.label4.TabIndex = 18;
             this.label4.Text = "Answer";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(366, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Calculate3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(472, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Calculate4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(578, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 23);
+            this.button5.TabIndex = 21;
+            this.button5.Text = "Calculate5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(366, 298);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 23);
+            this.button6.TabIndex = 22;
+            this.button6.Text = "Calculate3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(472, 298);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 23);
+            this.button7.TabIndex = 23;
+            this.button7.Text = "Calculate4";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(578, 298);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 23);
+            this.button8.TabIndex = 24;
+            this.button8.Text = "Calculate5";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 541);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -235,6 +297,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- Asignment2/Form1.cs
+++ Asignment2/Form1.cs
@@ -23,6 +23,70 @@ namespace Asignment2
             {
                 double A = double.Parse(textBox1.Text);
                 double B = double.Parse(textBox2.Text);
+                double ans = A + B;
+                label4.Text = ans.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a Number ");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double A = double.Parse(textBox6.Text);
+                double B = double.Parse(textBox7.Text);
+                double ans = 1 / (1 / A + 1 / B);
+                label3.Text = ans.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a Number"); 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double A = double.Parse(textBox1.Text);
+                double B = double.Parse(textBox2.Text);
+                double C = double.Parse(textBox3.Text);
+                double ans = A + B + C;
+                label4.Text = ans.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a Number ");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double A = double.Parse(textBox1.Text);
+                double B = double.Parse(textBox2.Text);
+                double C = double.Parse(textBox3.Text);
+                double D = double.Parse(textBox4.Text);
+                double ans = A + B + C + D;
+                label4.Text = ans.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a Number ");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double A = double.Parse(textBox1.Text);
+                double B = double.Parse(textBox2.Text);
                 double C = double.Parse(textBox3.Text);
                 double D = double.Parse(textBox4.Text);
                 double E = double.Parse(textBox5.Text);
@@ -31,11 +95,49 @@ namespace Asignment2
             }
             catch
             {
-                MessageBox.Show("Enter a Number "); 
+                MessageBox.Show("Enter a Number ");
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double A = double.Parse(textBox6.Text);
+                double B = double.Parse(textBox7.Text);
+                double C = double.Parse(textBox8.Text);
+                double ans = 1 / (1 / A + 1 / B + 1 / C);
+                label3.Text = ans.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a Number");
+            }
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double A = double.Parse(textBox6.Text);
+                double B = double.Parse(textBox7.Text);
+                double C = double.Parse(textBox8.Text);
+                double D = double.Parse(textBox9.Text);
+                double ans = 1 / (1 / A + 1 / B + 1 / C + 1 / D);
+                label3.Text = ans.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter a Number");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
             {
 
                 double A = double.Parse(textBox6.Text);
@@ -43,9 +145,14 @@ namespace Asignment2
                 double C = double.Parse(textBox8.Text);
                 double D = double.Parse(textBox9.Text);
                 double E = double.Parse(textBox10.Text);
-                double ans = 1 / 1 / A + 1 / B + 1 / C + 1 / D + 1 / E;
+                double ans = 1 / (1 / A + 1 / B + 1 / C + 1 / D + 1 / E);
                 label3.Text = ans.ToString();
-            }       
+            }
+            catch
+            {
+                MessageBox.Show("Enter a Number");
+            }
         }
     }
 }
+    
